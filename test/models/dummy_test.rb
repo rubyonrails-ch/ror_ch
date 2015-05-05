57f7321ebffd5fc93fbdbc1899d57235c1b7c7ce
Christian Felder (masone) <ema@rh-productions.ch>
2015-05-05 16:34:01 +0200

    dummy test setup

---
+++ test/models/dummy_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class DummyTest < ActiveSupport::TestCase
+
+  it "be green" do
+    assert true
+  end
+
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,23 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'minitest/rails'
+require 'minitest/reporters'
+require 'mocha/mini_test'
+
+Minitest::Reporters.use! [Minitest::Reporters::ProgressReporter.new]
+DatabaseCleaner[:mongoid].strategy = :truncation
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  def setup
+    DatabaseCleaner.clean # reset database
+    super
+  end
+
+  def teardown
+    ActionMailer::Base.deliveries.clear
+    super
+  end
+
 end
