6d9da95776d2b20d6b6c1f89997ce822cd70b25c
Christian Felder (masone) <ema@rh-productions.ch>
2015-05-08 11:09:08 +0200

    time with zone

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @events = Event.asc(:time).gt(time: Time.now.beginning_of_day)
+    @events = Event.asc(:time).gt(time: Time.zone.now.beginning_of_day)
   end
 end
