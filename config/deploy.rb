0970b08ef48ca4784e6984f9731eb3bc49ca7856
seb <seb@panter.ch>
2015-05-31 16:09:34 +0300

    config file handling for ungleich deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,8 +26,8 @@ set :log_level, :info
 # Default value for :pty is false
 # set :pty, true
 
-# Default value for linked_dirs is []
-set :linked_dirs, %w[log tmp/cache]
+set :linked_files, %w(config/database.yml config/secrets.yml)
+set :linked_dirs, %w(log tmp/cache)
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
