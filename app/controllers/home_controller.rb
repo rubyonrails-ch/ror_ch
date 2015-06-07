d4ee496a399934ea231b86260ed282ad3c97fa33
Dominique Schuwey <domi@domidesign.ch>
2015-06-07 21:34:04 +0200

    implement new design

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
+  skip_before_filter  :verify_authenticity_token
+
   def index
-    @events = Event.where('time > ?', Time.now.beginning_of_day).order(:time)
+    @events = Event.where('time > ?', Time.now.beginning_of_day).order(:time).limit(4)
+    @jobs = Job.all.limit(5).by_date
+  end
   end
 end
